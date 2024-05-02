fa6b9d0b245d1dba6340f02b18e00b1ace784812
hyneq <hyneqs@gmail.com>
2024-05-02 22:32:25 +0200

    Mediamtx uses config from env vars only

--- mediamtx/mediamtx_proxy.sh
+++ mediamtx/mediamtx_proxy.sh
@@ -2,7 +2,14 @@
 
 source "$(dirname "$0")/../vars"
 
-export MTX_WEBRTCADDRESS=":$MEDIAMTX_WEBRTC_PORT"
-export MTX_PATHS_PREDICTION_SOURCE="$REMOTE_STREAM_PATH"
+export MTX_RTSP=no
+export MTX_RTMP=no
+export MTX_HLS=no
+export MTX_SRT=no
 
-exec "$mediamtx_dir/mediamtx" "$mediamtx_dir/mediamtx.yml"
+export MTX_WEBRTCADDRESS=":$mediamtx_webrtc_port"
+
+export MTX_PATHS_PREDICTION_SOURCE="$REMOTE_STREAM_PATH"
+export MTX_PATHS_PREDICTION_SOURCEONDEMAND=yes
+
+exec "$mediamtx_dir/mediamtx"
