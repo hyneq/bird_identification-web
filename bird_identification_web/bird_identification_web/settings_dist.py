1b0ed1ff55f8d0ca94cbc3814aa2fa7c5e90f2d1
hyneq <hyneqs@gmail.com>
2024-05-03 01:59:42 +0200

    ALLOWED_HOSTS is defined in vars

--- bird_identification_web/bird_identification_web/settings_dist.py
+++ bird_identification_web/bird_identification_web/settings_dist.py
@@ -35,7 +35,7 @@ except FileNotFoundError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(',')
 
 
 # Application definition
