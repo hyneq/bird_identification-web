40c56538e3214444c227e8fa4e4a452089536311
hyneq <hyneqs@gmail.com>
2024-05-03 02:05:23 +0200

    Mediamtx_proxy now servers HLS instead of WebRTC

--- mediamtx/mediamtx_proxy.sh
+++ mediamtx/mediamtx_proxy.sh
@@ -4,10 +4,10 @@ source "$(dirname "$0")/../vars"
 
 export MTX_RTSP=no
 export MTX_RTMP=no
-export MTX_HLS=no
+export MTX_WEBRTC=no
 export MTX_SRT=no
 
-export MTX_WEBRTCADDRESS=":$mediamtx_webrtc_port"
+export MTX_HLSADDRESS=":$mediamtx_hls_port"
 
 export MTX_PATHS_PREDICTION_SOURCE="$REMOTE_STREAM_PATH"
 export MTX_PATHS_PREDICTION_SOURCEONDEMAND=yes
